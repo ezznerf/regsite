6cec63da50eb53cd7b3c1ed2bef1c8e189ba77fc
ezznerf <yakuta04@mail.ru>
2023-07-03 00:15:14 +0300

    Add some getters and setters

--- app/AbstractSaver.php
+++ app/AbstractSaver.php
@@ -7,4 +7,32 @@ abstract class AbstractSaver
     private $email;
     abstract public function writeToTextBase();
     abstract public function writeToSerialBase();
+    public function setName($name){
+        $this->name = $name;
+        return $this;
+    }
+    public function getName($name){
+        return $this -> name;
+    }
+    public function setBday($bday){
+        $this->name = $bday;
+        return $this;
+    }
+    public function getBday($bday){
+        return $this -> bday;
+    }
+    public function setPnumber($pnumber){
+        $this->name = $pnumber;
+        return $this;
+    }
+    public function getPnumber($pnumber){
+        return $this -> pnumber;
+    }
+    public function setEmail($email){
+        $this->name = $email;
+        return $this;
+    }
+    public function getEmail($email){
+        return $this -> email;
+    }
 }
