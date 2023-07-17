82504d2e9864bd1b5a7b1507b16ace4774a1b179
ezznerf <yakuta04@mail.ru>
2023-07-17 14:00:14 +0300

    rework for DB

--- main/text1.php
+++ main/text1.php
@@ -13,6 +13,13 @@
     require_once("../app/SaveToBase.php");
     $newPerson = new SaveToBase();
     $newPerson -> writeToTextBase();
+    require_once("../app/DBaseSaver.php");
+    $newUser = new DBaseSaver();
+    $uname = $_POST['name'];
+    $ubday = $_POST['bday'];
+    $uphone = $_POST['pnumber'];
+    $uemail = $_POST['email'];
+    $newUser->SQLSave("$uname", "$ubday", "$uphone", "$uemail");
     ?>
     <div id="alerts" class="alerts">
         <h2>Сохранение в текстовый файл прошло успешно<h2>
