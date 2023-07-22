fde1cf894f40f0064a5535429a0fe956ef0bdc72
ezznerf <yakuta04@mail.ru>
2023-07-22 23:54:59 +0300

    separate username

--- app/DBaseSaver.php
+++ app/DBaseSaver.php
@@ -1,13 +1,17 @@
 <?php
-require_once ('DBaseInterface.php');
 
-class DBaseSaver implements DBaseInterface
+
+class DBaseSaver
 {
    public function SQLSave($uname, $ubday, $uphone, $uemail)
    {
 
-       $mysql = new mysqli('localhost','root', 'root', 'regsite');
-       $mysql->query("INSERT INTO `users`(`name`, `bday`, `phone`, `email`)VALUES ('$uname', '$ubday', '$uphone', '$uemail')");
+       $mysql = new mysqli('localhost','root', '', 'regsite');
+       $fullName = explode(" ",$uname);
+       $name = $fullName[0];
+       $surname = $fullName[1];
+       $patronymic = $fullName[2];
+       $mysql->query("INSERT INTO `users`(`name`,`surname`,`patronymic`, `bday`, `pnumber`, `email`)VALUES ('$name','$surname','$patronymic','$ubday','$uphone','$uemail')");
 
        $mysql->close();
    }
